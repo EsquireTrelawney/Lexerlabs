6fc061832d146e26e55b6120eb784df043fe0fda
Trelawney <sqr.trelawney@gmail.com>
2024-04-04 02:07:20 +0300

    part of lexer for KubSU lessonsC#

---
+++ Lexer/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Lexer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxCode = new System.Windows.Forms.TextBox();
+            this.textBoxFinal = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.buttonStep = new System.Windows.Forms.Button();
+            this.buttonFull = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxCode
+            // 
+            this.textBoxCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxCode.Location = new System.Drawing.Point(6, 47);
+            this.textBoxCode.Multiline = true;
+            this.textBoxCode.Name = "textBoxCode";
+            this.textBoxCode.Size = new System.Drawing.Size(359, 454);
+            this.textBoxCode.TabIndex = 0;
+            // 
+            // textBoxFinal
+            // 
+            this.textBoxFinal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxFinal.Location = new System.Drawing.Point(371, 47);
+            this.textBoxFinal.Multiline = true;
+            this.textBoxFinal.Name = "textBoxFinal";
+            this.textBoxFinal.Size = new System.Drawing.Size(237, 454);
+            this.textBoxFinal.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.name});
+            this.dataGridView1.Location = new System.Drawing.Point(614, 47);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(168, 226);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // id
+            // 
+            this.id.HeaderText = "#";
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            this.id.Width = 25;
+            // 
+            // name
+            // 
+            this.name.HeaderText = "Name";
+            this.name.Name = "name";
+            this.name.ReadOnly = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2});
+            this.dataGridView2.Location = new System.Drawing.Point(614, 275);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.Size = new System.Drawing.Size(168, 226);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "#";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            this.dataGridViewTextBoxColumn1.Width = 25;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "Value";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.ReadOnly = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(110, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Исходный код";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(391, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(197, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Результат анализа";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(630, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Переменные";
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonOpen.Location = new System.Drawing.Point(788, 182);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(117, 28);
+            this.buttonOpen.TabIndex = 7;
+            this.buttonOpen.Text = "Открыть";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // buttonStep
+            // 
+            this.buttonStep.Enabled = false;
+            this.buttonStep.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStep.Location = new System.Drawing.Point(788, 245);
+            this.buttonStep.Name = "buttonStep";
+            this.buttonStep.Size = new System.Drawing.Size(117, 28);
+            this.buttonStep.TabIndex = 8;
+            this.buttonStep.Text = "Шаг";
+            this.buttonStep.UseVisualStyleBackColor = true;
+            this.buttonStep.Click += new System.EventHandler(this.buttonStep_Click);
+            // 
+            // buttonFull
+            // 
+            this.buttonFull.Enabled = false;
+            this.buttonFull.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonFull.Location = new System.Drawing.Point(788, 279);
+            this.buttonFull.Name = "buttonFull";
+            this.buttonFull.Size = new System.Drawing.Size(117, 28);
+            this.buttonFull.TabIndex = 9;
+            this.buttonFull.Text = "Полностью";
+            this.buttonFull.UseVisualStyleBackColor = true;
+            this.buttonFull.Click += new System.EventHandler(this.buttonFull_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(917, 531);
+            this.Controls.Add(this.buttonFull);
+            this.Controls.Add(this.buttonStep);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBoxFinal);
+            this.Controls.Add(this.textBoxCode);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxCode;
+        private System.Windows.Forms.TextBox textBoxFinal;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Button buttonStep;
+        private System.Windows.Forms.Button buttonFull;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+    }
+}
+
---
+++ Lexer/Form1.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Lexer
+{
+
+    public partial class Form1 : Form
+    {
+        //объявление глобальных переменных
+        public List<string> service;
+        public List<string> operators;
+        public List<string> separators;
+        public List<string> identificators;
+        public List<string> numbers;
+        public string code; //строка, хранящая изначальный код для анализа
+        public string lex; //строка хранящая конечный набор лексем
+        public Form1()
+        {
+            //Инициализируем компоненты
+            InitializeComponent();
+
+            service = new List<string>() { "C", "CALL", "CHARACTER", "DIMENSION", "END", "FUNCTION", "GOTO", "IF", "INTEGER", "PROGRAM", "REAL", "STOP", "SUBROUTINE" };
+            operators = new List<string>() { "+", "-", "*", "/", "**", ".GT.", ".LT.", ".GE.", ".LE.", ".NE.", ".EQ.", "=" };
+            separators = new List<string>() { " ", "(", ")", ",", "EOL"};
+            identificators = new List<string>(1);
+            numbers = new List<string>(1);
+
+            code = "";
+            lex = "";
+        }
+
+        //функция вызова шага анализатора
+        public string Analysis()
+        {
+            //bool finish = false; //логическая переменная отслеживающая завершение шага анализатора
+            bool flagid = false; //логическая переменная отслеживающая отношение текущей лексемы к идентификаторам
+            int iteration = 0; //счетчик иттераций работы анализатора
+            int length = code.Length; //текущая длина строки с анализируемым кодом
+            List <char> tokens = new List<char>(1); //список с символами для анализа
+            string lexem; //полученная после анализа лексическая единица
+            //MessageBox.Show(code);
+            if (length <= 3) return ("EoF"); //предпроверка на конец файла
+
+                tokens.Add(code[iteration]);
+
+            if ((tokens[0] >= 65) && (tokens[0] <= 90)) //первый вошедший символ - буква
+            {
+                while ((iteration+1<=length) && (((code[iteration + 1] >= 65) && (code[iteration + 1] <= 90)) || ((code[iteration + 1] >= 48) && (code[iteration + 1] <= 57)))) //добавляем символы лексемы, пока не встретим неподходящий символ
+                {    
+                    if ((code[iteration + 1] >= 48) && (code[iteration + 1] <= 57)) flagid = true;
+                    tokens.Add(code[++iteration]);
+                }
+
+                lexem = ListToStr(tokens); //конвертируем в строку полученный набор символов
+                if (flagid) //если в лексеме есть цифры, то ищем совпадения в идентификаторах, иначе добавляем новый
+                {
+                    int id = identificators.IndexOf(lexem);
+
+                    if (id == -1) //совпадений нет, добавляем новый идентификатор
+                    {
+                        identificators.Add(lexem);
+                        dataGridView1.Rows.Add(identificators.Count - 1, lexem);
+                        lexem = "I" + Convert.ToString(identificators.Count - 1);
+                        code = code.Substring(iteration+1);
+                        code = code.Trim();
+                        return (lexem);
+                    }
+                    else //совпадение в идентификаторах найдено
+                    {
+                        lexem = "I" + Convert.ToString(id);
+                        code = code.Substring(iteration+1);
+                        code = code.Trim();
+                        return (lexem);
+                    }
+                }
+                else //если лексема без цифр, то проверяем все варианты
+                {
+                    if (lexem == separators[4]) //если лексема разделитель конца строки
+                    {
+                        code = code.Substring(iteration + 1);
+                        code = code.Trim();
+                        return ("S4");
+                    }
+
+                    int id = service.IndexOf(lexem);
+                    if (id != -1) //если лексема служебное слово
+                    {
+                        lexem = "W" + Convert.ToString(id);
+                        code = code.Substring(iteration + 1);
+                        code = code.Trim();
+                        return (lexem);
+                    }
+
+                    id = identificators.IndexOf(lexem); //ищем лексему среди идентификаторов
+                    if (id == -1) //совпадений нет, добавляем новый идентификатор
+                    {
+                        identificators.Add(lexem);
+                        dataGridView1.Rows.Add(identificators.Count - 1, lexem);
+                        lexem = "I" + Convert.ToString(identificators.Count - 1);
+                        code = code.Substring(iteration + 1);
+                        code = code.Trim();
+                        return (lexem);
+                    }
+                    else //совпадение в идентификаторах найдено
+                    {
+                        lexem = "I" + Convert.ToString(id);
+                        code = code.Substring(iteration + 1);
+                        code = code.Trim();
+                        return (lexem);
+                    }
+                }
+                
+            }
+            else if ((tokens[0] >= 48) && (tokens[0] <= 57)) //первый символ - цифра
+            {
+                while ((iteration+1 <= length) && (((code[iteration+1] >= 48) && (code[iteration + 1] <= 57)) || (code[iteration + 1] == 46))) //Добавляем символы в лексему, пока не закончится файл, либо не попадем на разделитель (исключая точку)
+                    tokens.Add(code[++iteration]);
+
+                lexem = ListToStr(tokens); //конвертируем в строку полученный набор символов
+
+                int id = numbers.IndexOf(lexem); //ищем совпадения в числах
+                if (id == -1) //совпадений нет, создаем новое число
+                {
+                    numbers.Add(lexem);
+                    dataGridView2.Rows.Add(identificators.Count - 1, lexem);
+                    lexem = "N" + Convert.ToString(numbers.Count - 1);
+                    code = code.Substring(iteration + 1);
+                    code = code.Trim();
+                    return (lexem);
+                }
+                else //совпадение найдено, записываем лексему
+                {
+                    lexem = "N" + Convert.ToString(id);
+                    code = code.Substring(iteration + 1);
+                    code = code.Trim();
+                    return (lexem);
+                }
+
+            }
+            else if (tokens[0] == 46) //первый символ - точка
+            {
+                if ((code[iteration + 1] >= 48) && (code[iteration + 1] <= 57)) //следующий символ цифра
+                {
+                    while ((iteration + 1 <= length) && ((code[iteration + 1] >= 48) && (code[iteration + 1] <= 57))) //добавляем в лексему все цифры после точки
+                        tokens.Add(code[++iteration]);
+
+                    lexem = ListToStr(tokens); //конвертируем в строку полученный набор символов
+
+                    int id = numbers.IndexOf(lexem); //ищем совпадения в числах
+                    if (id == -1) //совпадений нет, создаем новое число
+                    {
+                        numbers.Add(lexem);
+                        dataGridView2.Rows.Add(identificators.Count - 1, lexem);
+                        lexem = "N" + Convert.ToString(numbers.Count - 1);
+                        code = code.Substring(iteration + 1);
+                        code = code.Trim();
+                        return (lexem);
+                    }
+                    else //совпадение найдено, записываем лексему
+                    {
+                        lexem = "N" + Convert.ToString(id);
+                        code = code.Substring(iteration + 1);
+                        code = code.Trim();
+                        return (lexem);
+                    }
+                }
+                else if ((iteration + 4 <= length) && (code[iteration + 1] >= 65) && (code[iteration + 1] <= 90))
+                {
+                    for (int i = iteration; iteration < i+3; )
+                        tokens.Add(code[++iteration]);
+
+                    lexem = ListToStr(tokens); //конвертируем в строку полученный набор символов
+
+                    int id = operators.IndexOf(lexem); //ищем совпадения в операторах
+                    if (id != -1) //если найдено выводим лексему
+                    {
+                        lexem = "O" + Convert.ToString(id);
+                        code = code.Substring(iteration + 1);
+                        code = code.Trim();
+                        return (lexem);
+                    }
+                    else return ("ERROR"); //если совпадений нет, то синтаксис программы неверен
+
+                }
+                else return ("ERROR");
+            }
+            else
+            {
+                while ((iteration + 1 <= length) && (((code[iteration + 1] >= 40) && (code[iteration + 1] <= 45)) || (code[iteration + 1] == 47) || (code[iteration+1] == 61)))
+                {
+                    tokens.Add(code[++iteration]);
+                }
+
+                lexem = ListToStr(tokens); //конвертируем в строку полученный набор символов
+
+                int id = separators.IndexOf(lexem); //ищем совпадения в разделителях
+                if (id != -1) //если лексема разделитель
+                {
+                    lexem = "S" + Convert.ToString(id);
+                    code = code.Substring(iteration + 1);
+                    code = code.Trim();
+                    return (lexem);
+                }
+
+                id = operators.IndexOf(lexem); //ищем совпадения в операторах
+                if (id != -1) //если лексема оператор
+                {
+                    lexem = "O" + Convert.ToString(id);
+                    code = code.Substring(iteration + 1);
+                    code = code.Trim();
+                    return (lexem);
+                }
+            }
+            return ("ERROR");
+        }
+
+        public bool StepAnalysis()
+        {
+            //вызов метода анализатора
+            string step = Analysis();
+            //проверка возвращаемого результата на возможное окончание кода
+            if (step == "ERROR")
+            {
+                MessageBox.Show("Найден необрабатываемый символ!");
+                return (false);
+            }
+            else if (step == "EoF")
+            {
+                MessageBox.Show("Код полностью проанализирован!");
+                buttonStep.Enabled = false;
+                buttonFull.Enabled = false;
+                return (false);
+            }
+            else
+            {
+                lex += step + " ";
+                textBoxFinal.Text += step + " ";
+                return (true);
+            }
+        }
+
+        public string ListToStr(List<char> list) //функция преобразования списка в строку
+        {
+            string str;
+            char[] array = list.ToArray();
+            str = new string(array);
+            return (str);
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            string temp; //временная переменная хранящая строку из файла
+
+            StreamReader f = new StreamReader("code.txt");
+
+            //обнуление и активация элементов формы
+            textBoxCode.Text = "";
+            textBoxFinal.Text = "";
+            buttonStep.Enabled = true;
+            buttonFull.Enabled = true;
+            //цикл считывания кода из файла
+            while (!f.EndOfStream)
+            {
+                temp = f.ReadLine();
+                code += temp.Trim() + " EOL "; //заполнение рабочей строки
+                textBoxCode.Text += temp + Environment.NewLine; //заполнение окна вывода пользователю
+            }
+            code += "EOF";
+            f.Close();
+
+            //удаление пробелов в строке кода и перевод символов в верхний регистр
+            //string[] partstr = code.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            //code = String.Join("", partstr);
+            code = code.ToUpper();
+        }
+
+        //метод пошагового анализа
+        private void buttonStep_Click(object sender, EventArgs e)
+        {
+            StepAnalysis();
+        }
+
+        private void buttonFull_Click(object sender, EventArgs e)
+        {
+            bool flag = true;
+            while(flag)
+            {
+             flag = StepAnalysis();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
